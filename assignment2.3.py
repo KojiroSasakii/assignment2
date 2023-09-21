1dfd5d5964ea255e31ea8b348f0757a4672748f9
Mashrur Hossain <86108568+KojiroSasakii@users.noreply.github.com>
2023-09-21 09:50:00 +0900

    Update assignment2.3.py

--- assignment2.3.py
+++ assignment2.3.py
@@ -1,7 +1,7 @@
-sec = input("seconds")
-sec = int(sec)
-hours = sec // 3600 
-seconds = sec % 3600
-minutes = sec // 60
-seconds = sec % 60
-print (sec, "seconds is", hours, "hours",minutes, "minutes", seconds, "seconds")
+user_input = input("Enter seconds: ")
+user_input = int(user_input)
+hours = user_input // 3600 
+seconds = user_input % 3600
+minutes = user_input // 60
+seconds = user_input % 60
+print (user_input, "seconds is", hours, "hours",minutes, "minutes", seconds, "seconds")
